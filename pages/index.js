aa570b55c8cd676f164ecca2408bfb1e8fe1679d
latz09 <85132328+latz09@users.noreply.github.com>
2023-01-04 10:41:08 -0600

    ff

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,13 @@ import Hero from '../components/layouts/Landing/Hero';
 import StylistsLayout from '../components/Stylist/StylistsLayout';
 import connectToDatabase from '../lib/mongodb';
 
-export default function Home({stylists}) {
+export default function Home({ stylists }) {
 	// console.log(stylists)
 	return (
-		<div className='grid gap-16 mb-24 '>
-			<div className=''>
-				<Hero />
-			</div>
+		<div className='grid gap-16 mb-24 place-items-center'>
+			<Hero />
 			<div>
-				<StylistsLayout data={stylists}/>
+				<StylistsLayout data={stylists} />
 			</div>
 		</div>
 	);
@@ -20,9 +18,9 @@ export default function Home({stylists}) {
 // h-screen snap-y snap-mandatory overflow-scroll [&>*]:snap-center [&>*]:h-screen scroll scroll-smooth scrollbar-hide
 export async function getStaticProps() {
 	const client = await connectToDatabase;
-	const db = client.db("salon");
+	const db = client.db('salon');
 	const stylistsCollection = db.collection('stylists');
-	const data = await stylistsCollection.find().toArray()
+	const data = await stylistsCollection.find().toArray();
 
 	return {
 		props: { stylists: JSON.parse(JSON.stringify(data)) },
